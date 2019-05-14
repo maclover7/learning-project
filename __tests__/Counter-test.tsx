2beebb739ee24c53bafc9bebe72528643135abb0
Jon Moss <jonathan.moss@showclix.com>
2019-05-14 12:23:01 -0400

    Extract common setup code out

--- __tests__/Counter-test.tsx
+++ __tests__/Counter-test.tsx
@@ -13,38 +13,36 @@ configure({ adapter: new Adapter() });
 // Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer';
 
+interface createCounterCb { (component: Counter): void }
+const createCounter = (componentButtonText: string, cb: createCounterCb) => {
+  const wrapper = shallow(<Counter />);
+  const instance = wrapper.instance() as Counter;
+  const spy = jest.spyOn(instance, 'changeCount');
+
+  expect(instance.state.count).toBe(0);
+
+  instance.forceUpdate();
+  wrapper.find({
+    title: componentButtonText
+  }).props().onPress();
+
+  expect(spy).toHaveBeenCalledTimes(1);
+
+  cb(instance);
+};
+
 it('renders correctly', () => {
   renderer.create(<Counter />);
 });
 
 it ('decreases count', () => {
-  const wrapper = shallow(<Counter />);
-  const instance = wrapper.instance() as Counter;
-  const spy = jest.spyOn(instance, 'changeCount');
-
-  expect(instance.state.count).toBe(0);
-
-  instance.forceUpdate();
-  wrapper.find({
-    title: "Decrease Count"
-  }).props().onPress();
-
-  expect(spy).toHaveBeenCalledTimes(1);
-  expect(instance.state.count).toBe(-1);
+  createCounter("Decrease Count", (component: Counter) => {
+    expect(component.state.count).toBe(-1);
+  });
 });
 
 it ('increases count', () => {
-  const wrapper = shallow(<Counter />);
-  const instance = wrapper.instance() as Counter;
-  const spy = jest.spyOn(instance, 'changeCount');
-
-  expect(instance.state.count).toBe(0);
-
-  instance.forceUpdate();
-  wrapper.find({
-    title: "Increase Count"
-  }).props().onPress();
-
-  expect(spy).toHaveBeenCalledTimes(1);
-  expect(instance.state.count).toBe(1);
+  createCounter("Increase Count", (component: Counter) => {
+    expect(component.state.count).toBe(1);
+  });
 });
