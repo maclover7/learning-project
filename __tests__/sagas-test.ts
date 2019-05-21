a567b49f9b8c26d0f3889040347a0c5e1ccd094c
Jon Moss <jonathan.moss@showclix.com>
2019-05-21 09:37:01 -0400

    Run prettier

--- __tests__/sagas-test.ts
+++ __tests__/sagas-test.ts
@@ -4,15 +4,26 @@ import { getCounters, getCountersSuccess } from '../src/actions';
 import fetchMock from 'fetch-mock';
 
 it('will fetch counters - success', async () => {
-  const expectedCounters = [{ count: 6 }, { count: 4 }, { count: 2 }, { count: 1 }];
+  const expectedCounters = [
+    { count: 6 },
+    { count: 4 },
+    { count: 2 },
+    { count: 1 },
+  ];
   fetchMock.mock('https://api.myjson.com/bins/ywmke', expectedCounters);
 
   const dispatched: any[] = [];
-  const result = await runSaga({
-    dispatch: (action) => { dispatched.push(action) },
-  }, onGetCounters, getCounters()).toPromise();
+  const result = await runSaga(
+    {
+      dispatch: action => {
+        dispatched.push(action);
+      },
+    },
+    onGetCounters,
+    getCounters(),
+  ).toPromise();
 
-  expect(dispatched).toEqual([ getCountersSuccess(expectedCounters) ]);
+  expect(dispatched).toEqual([getCountersSuccess(expectedCounters)]);
 
   fetchMock.restore();
 });
